eca3b154ce4711d238e391ccd21a3cc7eb54f33a
Barragan <bdbs0996@gmail.com>
2021-08-26 13:00:09 -0500

    cualquier cosa

---
+++ views/includes/admin/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>INSPINIA | Dashboard</title>
+
+    <link href="../public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../public/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <!-- Toastr style -->
+    <link href="../public/css/plugins/toastr/toastr.min.css" rel="stylesheet">
+
+    <!-- Gritter -->
+    <link href="../public/js/plugins/gritter/jquery.gritter.css" rel="stylesheet">
+
+    <link href="../public/css/animate.css" rel="stylesheet">
+    <link href="../public/css/style.css" rel="stylesheet">
+
+</head>
