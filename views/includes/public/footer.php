cf835e03e71b14c51409ce8b8a2bf233396d6d6a
Denis <jsherr.6@gmail.com>
2021-08-28 10:23:05 -0500

    modificaciones en el CSS y HTML

--- views/includes/public/footer.php
+++ views/includes/public/footer.php
@@ -16,10 +16,11 @@
     </div>
     <div class="col-lg-6">
         <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
-        <a href="#intro" class="scrollto">Home</a>
-        <a href="#about" class="scrollto">About</a>
-        <a href="#">Privacy Policy</a>
-        <a href="#">Terms of Use</a>
+        <a href="#intro" class="scrollto">Inicio</a>
+        <a href="#about" class="scrollto">Sobre Nosotros</a>
+        <a href="#">Ciclos</a>
+        <a href="#">Matricula Virtual</a>
+        <a href="#">Contacto</a>
         </nav>
     </div>
     </div>
--- views/includes/public/head.php
+++ views/includes/public/head.php
@@ -5,13 +5,14 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Vesperr Bootstrap Template - Index</title>
+  <title>ACADEMY</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="public/web/assets/img/favicon.png" rel="icon">
-  <link href="public/web/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+ <!-- Favicons -->
+ <link href="public/web/assets/img/logo.svg" rel="icon">
+  <!-- <link href="../../../public/img/logo.svg" rel="icon"> -->
+  <!-- <link href="public/web/assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
 
   <!-- Google Fonts -->
   <link href="public/web/https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
