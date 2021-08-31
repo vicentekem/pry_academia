3065760e93b22b4b67b4457bfb5f44c018d306a6
joel <joel_vicente_23@hotmail.com>
2021-08-31 16:24:58 -0500

    procedures y login

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -28,19 +28,14 @@ class UsuarioController
         $action = isset($_GET['action']) ? $_GET['action'] : "";
 
         switch ($action){
-            case "qry_usuarios": $array_result = $this->validator->getUsuarios();break;
-            case "qry_login_usuario": $array_result = $this->validator->getLoginUsuario();break;
-            case "cbx_dependencias": $array_result = $this->validator->getCbxDependencias();break;
-            case "cbx_personal": $array_result = $this->validator->getCbxPersonal();break;
-            case "cbx_cargos": $array_result = $this->validator->getCbxCargos();break;
-            case "cbx_roles": $array_result = $this->validator->getCbxRoles();break;
 
-            case "check_dni_user": $array_result = $this->validator->checkDniUser();break;
+            case "qry_usuarios": $array_result = $this->validator->getUsuarios();break;
+            
             default:
                 $array_result["error"] = "Error al procesar la petición";
                 break;
         }
-
+        
         echo json_encode( $array_result );
 
     }
@@ -54,18 +49,17 @@ class UsuarioController
                 if( $array_result["error"] == "" ){
                     @session_start();
                     $_SESSION['usuario_academia'] = $array_result["row"];
-                    $url_bak = isset($_POST['url_bak']) ? $_POST['url_bak'] : '';
-                    $url = $url_bak ? $url_bak : 'dashboard';
-                    header('location:../?url=' . $url);
+                    //$url_bak = isset($_POST['url_bak']) ? $_POST['url_bak'] : '';
+                    //$url = $url_bak ? $url_bak : 'dashboard';
+                    header('location:../?url=dashboard');
                 }else{
                     header('location:../?url=login');
                 }
                 break;
+                
             case "login_ajax": $array_result = $this->validator->login();break;
-            case "change_pass": $array_result = $this->validator->changePass();break;
-            case "change_pass_req": $array_result = $this->validator->changePassReq();break;
-            case "send_email_cod_seg": $array_result = $this->validator->sendEmailCodSeg();break;
-            case "valid_cod_seg": $array_result = $this->validator->validCodSeg();break;
+            
+
             default:
                 $array_result["error"] = "Error al procesar la petición";
         }
