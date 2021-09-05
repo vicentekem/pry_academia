1df1b5b790bac058169fe27f02d6afdebd360908
Denis <jsherr.6@gmail.com>
2021-09-05 18:23:54 -0500

    queda

--- views/ciclos.php
+++ views/ciclos.php
@@ -24,10 +24,6 @@
 
 
             <button type="submit" class="searchButton">
-              <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-</svg> -->
-
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="#b7defa" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <circle cx="10" cy="10" r="7" />
