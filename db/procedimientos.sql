228639990e1b4c9360e7062bea3927dd9db21ba7
Barragan <bdbs0996@gmail.com>
2021-09-07 17:51:09 -0500

    sp_usuario

--- db/procedimientos.sql
+++ db/procedimientos.sql
@@ -133,14 +133,25 @@ _usuario VARCHAR(50),
 _password varchar(150),
 _id_usuario VARCHAR(32))
 BEGIN
+DECLARE msg varchar(255) default '';
+ set msg = CONCAT('El usuario ', _usuario , ' ya existe' );
 case _action 
  WHEN 'ins' THEN
+	IF(SELECT id from tbl_usuario where usuario<>_usuario) is NULL then
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg, MYSQL_ERRNO = 1001;
+		ELSEIF(SELECT id from tbl_usuario where id_persona<>_id_persona) IS NULL THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ya existe una cuenta con este DNI', MYSQL_ERRNO = 1001;
+		ELSE
+	INSERT INTO tbl_usuario(id,id_persona,usuario,`password`,create_at,user_create_at,update_password)
+				VALUES(_id,_id_persona,_usuario,MD5(_password),CURRENT_TIMESTAMP,_id_usuario,0);
+		END if;
+		/*
 	IF(SELECT id from tbl_usuario where id_persona=_id_persona OR usuario=_usuario LIMIT 1) IS NULL THEN
 	INSERT INTO tbl_usuario(id,id_persona,usuario,`password`,create_at,user_create_at,update_password)
 				VALUES(_id,_id_persona,_usuario,MD5(_password),CURRENT_TIMESTAMP,_id_usuario,0);
 	ELSE
 	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El usuario ya existe' , MYSQL_ERRNO = 1001;	
-	END if;
+	END if;*/
  WHEN'upd' THEN
 	IF(SELECT id from tbl_usuario where id_persona=_id_persona and id<>_id) is NULL THEN
 	UPDATE tbl_usuario SET usuario=_usuario,`password`=MD5(_password),create_up=CURRENT_TIMESTAMP,user_create_up=_id_usuario,update_password=1 WHERE id=_id and id_persona=_id_persona;
