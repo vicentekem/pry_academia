1a068cc131d20350b1e04b1bf9b0055a22ed8eb3
Denis <jsherr.6@gmail.com>
2021-09-06 20:24:13 -0500

    vista de ciclos descripion

--- views/temarios.php
+++ views/temarios.php
@@ -6,12 +6,12 @@
 
     <!-- ================================================================= -->
     <!-- contenido temario -->
-    <div class="caja__contenido">
+    <div class="caja__contenido--temario">
         <div class="caja__flex">
             <!-- contenido -->
             <div class="caja__width">
                 <!-- titulo curso -->
-                <h2 class="caja__title">Kubernetes desde cero</h2>
+                <h2 class="caja__title">Aritmetica desde cero</h2>
                 <p class="caja__description  curso__text ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, cum. Facere tempora in deleniti </p>
 
                 <!-- assets -->
@@ -50,7 +50,10 @@
 
                     <!-- duracion -->
                     <div class="curso__text curso__flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 512.009 512.009" class="svg-icon svg__nivel fill normal yellow-400 s-mr-1" id="" fill="currentColor" title=""><path d="M272.392,249.22l-0.009-132.207c0-9.052-7.339-16.383-16.383-16.383s-16.383,7.331-16.383,16.383L239.626,256c0,4.349,1.731,8.511,4.801,11.59l85.765,85.765c3.204,3.195,7.393,4.792,11.59,4.792c4.189,0,8.378-1.589,11.573-4.792c6.399-6.399,6.399-16.764,0-23.172L272.392,249.22z"></path><path d="M512.009,256.009c0-0.079-0.011-0.155-0.012-0.234C511.87,114.712,397.083,0.009,256,0.009c-141.161,0-256,114.839-256,256S114.839,512,256,512c141.083,0,255.87-114.695,255.997-255.757C511.998,256.164,512.009,256.088,512.009,256.009z M272.383,478.64v-25.471c0-9.052-7.339-16.383-16.383-16.383s-16.383,7.331-16.383,16.383v25.471c-110.061-8.023-198.227-96.196-206.248-206.257h25.471c9.052,0,16.383-7.322,16.383-16.374c0-9.052-7.331-16.383-16.383-16.383H33.368c8.018-110.065,96.185-198.235,206.249-206.257v25.471c0,9.043,7.339,16.383,16.383,16.383c9.052,0,16.383-7.339,16.383-16.383V33.369c110.068,8.022,198.227,96.195,206.249,206.257h-25.462c-9.052,0-16.392,7.322-16.392,16.374c0,9.052,7.339,16.383,16.392,16.383h25.462C470.615,382.448,382.454,470.619,272.383,478.64z"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.25em" height="1.25em" viewBox="0 0 512.009 512.009" class="svg-icon svg__nivel fill normal yellow-400 s-mr-1" id="" fill="currentColor" title="">
+                            <path d="M272.392,249.22l-0.009-132.207c0-9.052-7.339-16.383-16.383-16.383s-16.383,7.331-16.383,16.383L239.626,256c0,4.349,1.731,8.511,4.801,11.59l85.765,85.765c3.204,3.195,7.393,4.792,11.59,4.792c4.189,0,8.378-1.589,11.573-4.792c6.399-6.399,6.399-16.764,0-23.172L272.392,249.22z"></path>
+                            <path d="M512.009,256.009c0-0.079-0.011-0.155-0.012-0.234C511.87,114.712,397.083,0.009,256,0.009c-141.161,0-256,114.839-256,256S114.839,512,256,512c141.083,0,255.87-114.695,255.997-255.757C511.998,256.164,512.009,256.088,512.009,256.009z M272.383,478.64v-25.471c0-9.052-7.339-16.383-16.383-16.383s-16.383,7.331-16.383,16.383v25.471c-110.061-8.023-198.227-96.196-206.248-206.257h25.471c9.052,0,16.383-7.322,16.383-16.374c0-9.052-7.331-16.383-16.383-16.383H33.368c8.018-110.065,96.185-198.235,206.249-206.257v25.471c0,9.043,7.339,16.383,16.383,16.383c9.052,0,16.383-7.339,16.383-16.383V33.369c110.068,8.022,198.227,96.195,206.249,206.257h-25.462c-9.052,0-16.392,7.322-16.392,16.374c0,9.052,7.339,16.383,16.392,16.383h25.462C470.615,382.448,382.454,470.619,272.383,478.64z"></path>
+                        </svg>
                         <p>Duración: 1-año</p>
                     </div>
                 </div>
@@ -128,8 +131,81 @@
                 </div>
             </div>
         </div>
-    </div>
 
+        <!-- titulo -->
+        <div class="contenido--title">
+            <h2>¡Nuevos Inicios!</h2>
+        </div>
+
+        <!-- baner -->
+        <div class="contenedor__banner">
+            <div class="banner__flex">
+                <div class="banner__pre">
+                    <div class="banner__pre--caja">
+                        <h2>Preparacion ciclo escolar</h2>
+                    </div>
+                </div>
+                <div class="banner__inicio">
+                    <div class="banner__inicio--caja">
+                        <p>inicio: 09 de setiembre</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Progama -->
+        <div class="programa__container">
+            <div class="programa__caja">
+                <div class="programa__title">
+                    <h2>Lo que otorgamamos </h2>
+                </div>
+
+                <div class="programa__beni">
+                    <!-- ===================== -->
+                    <div class="beneficio">
+                        <div class="beneficio__title">
+                            <h3>Beneficios</h3>
+                        </div>
+                        <div class="beneficio__caja">
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- ====================== -->
+                    <div class="beneficio">
+                        <div class="beneficio__title">
+                            <h3>Caracteristicas</h3>
+                        </div>
+                        <div class="beneficio__caja">
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
@@ -139,21 +215,21 @@
     <?php include "views/includes/public/footer.php"; ?>
 
 
-<!-- Vendor JS Files -->
-<script src="public/web/assets/vendor/aos/aos.js"></script>
-<script src="public/web/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="public/web/assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="public/web/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="public/web/assets/vendor/php-email-form/validate.js"></script>
-<script src="public/web/assets/vendor/purecounter/purecounter.js"></script>
-<script src="public/web/assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <!-- Vendor JS Files -->
+    <script src="public/web/assets/vendor/aos/aos.js"></script>
+    <script src="public/web/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="public/web/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="public/web/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="public/web/assets/vendor/php-email-form/validate.js"></script>
+    <script src="public/web/assets/vendor/purecounter/purecounter.js"></script>
+    <script src="public/web/assets/vendor/swiper/swiper-bundle.min.js"></script>
 
-<!-- Template Main JS File -->
-<script src="public/web/assets/js/main.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="public/web/assets/js/header.js"></script>
-<script src="public/web/assets/js/header/header.js"></script>
-<!-- <script src="public/web/assets/js/header/jquery.min.js"></script> -->
-<script src="public/web/assets/js/header/main.js"></script>
+    <!-- Template Main JS File -->
+    <script src="public/web/assets/js/main.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="public/web/assets/js/header.js"></script>
+    <script src="public/web/assets/js/header/header.js"></script>
+    <!-- <script src="public/web/assets/js/header/jquery.min.js"></script> -->
+    <script src="public/web/assets/js/header/main.js"></script>
 
 </body>
