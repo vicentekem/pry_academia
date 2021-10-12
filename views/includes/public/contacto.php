cb05410192eb1f91775029cacd8be9259b2cad2b
Denis <jsherr.6@gmail.com>
2021-10-12 08:58:58 -0500

    cambios concat

--- views/includes/public/contacto.php
+++ views/includes/public/contacto.php
@@ -1,73 +1,51 @@
 <!-- ======= Contact Section ======= -->
 <section id="contact" class="contact space--contactos">
-      <div class="container">
+  <div class="container">
 
-        <div class="section-title" ">
+    <div class="section-title" ">
           <h2>ACADEMY</h2>
         </div>
 
-        <div class="row">
+        <div class=" row row-caja">
 
-          <div class="col-lg-4 col-md-6" " data-aos-delay="100">
-            <div class="contact-about">
-              <h3>Academy</h3>
-              <p>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-              <div class="social-links">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
+      <div class="col-lg-4 col-md-6" " data-aos-delay=" 100">
+        <div class="contact-about">
+          <div>
+            <h3>Academy</h3>
+            <p>Cras fermentum odio eu feugiat. Justo eget magna fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+          </div>
+          <div class="social-links">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mt-4 mt-md-0 comtact" data-aos-delay="200">
+        <div class="info">
+          <div>
+            <i class="ri-map-pin-line"></i>
+            <p>A108 Adam Street<br>NY 535022</p>
           </div>
 
-          <div class="col-lg-3 col-md-6 mt-4 mt-md-0" " data-aos-delay="200">
-            <div class="info">
-              <div>
-                <i class="ri-map-pin-line"></i>
-                <p>A108 Adam Street<br>NY 535022</p>
-              </div>
-
-              <div>
-                <i class="ri-mail-send-line"></i>
-                <p>info@example.com</p>
-              </div>
-
-              <div>
-                <i class="ri-phone-line"></i>
-                <p>+51 926 540 928</p>
-              </div>
-
-            </div>
+          <div>
+            <i class="ri-mail-send-line"></i>
+            <p>info@example.com</p>
           </div>
 
-          <div class="col-lg-5 col-md-12" " data-aos-delay="300">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Tus nombres" required>
-              </div>
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="lastname" placeholder="Tus apellidos" required>
-              </div>
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="dni" placeholder="DNI" required>
-              </div>
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="telefono" placeholder="Telefono" required>
-              </div>
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="celular" placeholder="Celular" required>
-              </div>
-              <div class="form-group">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Tu correro" required>
-              </div>
-              <div id="contactos" class="text-center btn-space"><button class="cambios--boton" type="submit">Send Message</button></div>
-            </form>
+          <div>
+            <i class="ri-phone-line"></i>
+            <p>+51 926 540 928</p>
           </div>
 
         </div>
-
       </div>
-    </section><!-- End Contact Section -->
 
-   
+
+
+    </div>
+
+  </div>
+</section><!-- End Contact Section -->
